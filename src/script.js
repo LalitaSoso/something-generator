20d8a6c5876a7a045efd86fc40767e9571b1533d
LalitaSoso <127132789+LalitaSoso@users.noreply.github.com>
2024-07-31 15:00:48 -0400

    added AI integration

--- src/script.js
+++ src/script.js
@@ -1,13 +1,29 @@
+function displayAffirmation(response){
+    console.log("affirmation generated");
+
+new Typewriter("#affirmation", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 20,
+    cursor: null,
+  });
+}
+
 function generateAffirmation(event) {
 event.preventDefault();
 
-new Typewriter("#affirmation", {
-    strings: "And, when you want something, all the universe conspires in helping you to achieve it.",
-    autoStart: true,
-    delay: 30,
-    cursor: "",
-  });
+let userInput = document.querySelector("#user-input");
+let apiKey = "d825bd2a96813oc03e814ff3atbaecf8";
+let prompt = `User instruction: Generate a positive affirmation using ${userInput.value}`;
+let context =
+"You are a kind person who believes in your ability to project and manifest your future. You create quality affirmations using the user instructions. The affirmation must be provided in HTML format using <br /> if there is more than one sentence. Example: <p>this is an affirmation. <br /> This continues to be an affirmation</p>";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+
+console.log("Generating an affirmation");
+console.log(`Prompt: ${prompt}`);
+console.log(`Context: ${context}`);
+axios.get(apiUrl).then(displayAffirmation);
 }
 
 let affirmationFormElement = document.querySelector("#affirmation-generator-form");
